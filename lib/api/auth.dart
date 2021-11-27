47b8803055e08b0a804d8a93ae207ab4f1c26bca
ShortyDev <jakob123413@gmail.com>
2021-11-27 01:05:43 +0100

    added client certificate

--- lib/api/auth.dart
+++ lib/api/auth.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:math';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
 import 'package:operuit_flutter/util/cryptoop.dart';
 import 'package:overlay_support/overlay_support.dart';
 
@@ -27,66 +28,80 @@ class Auth extends StatelessWidget {
 
   const Auth({Key? key}) : super(key: key);
 
-  Future<http.Response> _post(String path, String data) {
-    return http.post(Uri.parse(_uri + path),
-        body: json.decode(data), headers: _headers);
+  Future<String> readResponse(HttpClientResponse response) async {
+    return response.transform(utf8.decoder).join();
   }
 
-  Future<http.Response> _get(String path, String dataStr) {
-    return http.get(Uri.parse(_uri + path + "?" + dataStr),
-        headers: _headers);
+  Future<HttpClientResponse> _post(String path, String data) async {
+    ByteData cert = await rootBundle.load("assets/rootCA.pfx");
+    final client = HttpClient(context: SecurityContext(withTrustedRoots: true)
+      ..useCertificateChainBytes(cert.buffer.asUint8List(), password: 'FLh4ZQaRQwdqhG6F')
+      ..usePrivateKeyBytes(cert.buffer.asUint8List(), password: 'FLh4ZQaRQwdqhG6F'));
+    final request = await client.postUrl(Uri.parse(_uri + path));
+    _headers.forEach((key, value) => {request.headers.set(key, value)});
+    request.add(utf8.encode(json.decode(data)));
+    final response = await request.close();
+    return response;
   }
 
-  Future<int> register(String username, String displayName, String password) async {
+  Future<HttpClientResponse> _get(String path, String dataStr) async {
+    ByteData cert = await rootBundle.load("assets/rootCA.pfx");
+    final client = HttpClient(context: SecurityContext(withTrustedRoots: true)
+      ..useCertificateChainBytes(cert.buffer.asUint8List(), password: 'FLh4ZQaRQwdqhG6F')
+      ..usePrivateKeyBytes(cert.buffer.asUint8List(), password: 'FLh4ZQaRQwdqhG6F'));
+    final request = await client.getUrl(Uri.parse(_uri + path + "?" + dataStr));
+    _headers.forEach((key, value) => {request.headers.set(key, value)});
+    final response = await request.close();
+    return response;
+  }
+
+  Future<int> register(
+      String username, String displayName, String password) async {
     var value = await _post(
-            'register',
-            jsonEncode(
-                "{\"username\":\"$username\", \"displayName\":\"$displayName\", \"password\":\"$password\"}"));
+        'register',
+        jsonEncode(
+            "{\"username\":\"$username\", \"displayName\":\"$displayName\", \"password\":\"$password\"}"));
     if (value.statusCode != 200) {
-      lastRequest = value.headers.entries.where((element) => element.key.toLowerCase() == "cf-ray").first.value;
+      lastRequest = value.headers.value("cf-ray")!;
       showSimpleNotification(
           Text(
               "An unknown error occurred, please try again.\nCF-Ray: $lastRequest"),
           background: Colors.red,
-          position:
-          NotificationPosition.bottom);
+          position: NotificationPosition.bottom);
     }
-    var result = jsonDecode(value.body);
+    var contents = await readResponse(value);
+    var result = jsonDecode(contents);
     return result["code"];
   }
 
   Future<bool> login(String username, String password) async {
-    var value = await _post(
-        'login',
-        jsonEncode(
-            "{\"username\":\"$username\", \"password\":\"$password\"}"));
+    var value = await _post('login',
+        jsonEncode("{\"username\":\"$username\", \"password\":\"$password\"}"));
     if (value.statusCode != 200) {
-      lastRequest = value.headers.entries.where((element) => element.key.toLowerCase() == "cf-ray").first.value;
+      lastRequest = value.headers.value("cf-ray")!;
       showSimpleNotification(
           Text(
               "An unknown error occurred, please try again.\nCF-Ray: $lastRequest"),
           background: Colors.red,
-          position:
-          NotificationPosition.bottom);
+          position: NotificationPosition.bottom);
     }
-    var result = jsonDecode(value.body);
+    var contents = await readResponse(value);
+    var result = jsonDecode(contents);
     return result["code"] == 200;
   }
 
   Future<String> salt(String username) async {
-    var value = await _get(
-        'salt',
-            "username=$username");
+    var value = await _get('salt', "username=$username");
     if (value.statusCode != 200) {
-      lastRequest = value.headers.entries.where((element) => element.key.toLowerCase() == "cf-ray").first.value;
+      lastRequest = value.headers.value("cf-ray")!;
       showSimpleNotification(
           Text(
               "An unknown error occurred, please try again.\nCF-Ray: $lastRequest"),
           background: Colors.red,
-          position:
-          NotificationPosition.bottom);
+          position: NotificationPosition.bottom);
     }
-    var result = jsonDecode(value.body);
+    var contents = await readResponse(value);
+    var result = jsonDecode(contents);
     return result["code"] == 200 ? result["message"] : "";
   }
 
